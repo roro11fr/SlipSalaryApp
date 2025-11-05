7dfb4e6a9ce121b054df3f291995144e1226bb09
roro11fr <franciuc455@gmail.com>
2025-11-05 10:56:43 +0200

    add main endpoints

--- slipApp/urls.py
+++ slipApp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ContractViewSet, PayrollPeriodViewSet, AuditFileViewSet
+from .views_aggregate import (
+    CreateAggregatedEmployeeDataView,
+    SendAggregatedEmployeeDataView,
+    CreatePdfForEmployeesView,
+    SendPdfToEmployeesView,
+)
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
@@ -10,4 +16,8 @@ router.register(r"audit-files", AuditFileViewSet, basename="audit-files")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("createAggregatedEmployeeData", CreateAggregatedEmployeeDataView.as_view()),
+    path("sendAggregatedEmployeeData", SendAggregatedEmployeeDataView.as_view()),
+    path("createPdfForEmployees", CreatePdfForEmployeesView.as_view()),
+    path("sendPdfToEmployees", SendPdfToEmployeesView.as_view()),
 ]
